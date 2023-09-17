77881e73bb773b33672ff1125ddb06ac7b6fb95b
Wojciech Kesek <wojtekesek@gmail.com>
2023-09-17 11:43:36 +0200

    validation fix

--- src/main/java/com/sda/travelagency/advice/GlobalException.java
+++ src/main/java/com/sda/travelagency/advice/GlobalException.java
@@ -4,6 +4,7 @@ import com.sda.travelagency.exception.*;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,5 +32,13 @@ public class GlobalException {
     @ExceptionHandler(CityNotFoundException.class)
     public ProblemDetail handleCityNotFoundException(CityNotFoundException e){
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        StringBuilder errors = new StringBuilder();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.append(error.getField()).append(": ").append(error.getDefaultMessage()).append(", ");
+        });
+        return new ResponseEntity<>("Validation error: " + errors.toString(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/sda/travelagency/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.sda.travelagency.exception;
-
-public class ValidationException extends RuntimeException{
-    public ValidationException(String message) {
-        super(message);
-    }
-}
